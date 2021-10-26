01345351ec0750252d12fead469d586a25488c53
saumya-2259 <saumyas.20cse@kongu.edu>
2021-10-26 19:36:32 +0530

    course complete

---
+++ coursedetail.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	int roll_no=21,sem=3,one_credit=1,value_added=1;
+	char onecredit_name[50]="git",valueadded_name[50]="datalake";
+	printf("ROLL NO:%d",roll_no);
+	printf("semester:%d",sem);
+	printf("no of one credit courses completed:%d",one_credit);
+	printf("name of one credit course:%c",onecredit_name);
+	printf("no of value added courses completed:%d",value_added);
+	printf("name of value added courses:%c",valueadded_name);
+}
+
